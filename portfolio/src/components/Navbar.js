8adb4740e610dcf307980c940d9cf65b3eac8cc0
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-03 04:52:59 +0200

    made navbar mobile friendly

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
    return (
@@ -9,14 +10,13 @@ const Navbar = () => {
             <a target="_blank" rel="github-link" href="https://github.com/iMMoCodes"><FontAwesomeIcon className="github-icon" icon={ faGithub }/></a>
             <h1>Github</h1>
          </div>
-         <div className="nav-list-container">
-            <ul>
-               <li><a href="#home">Home</a></li>
-               <li><a href="#about">About</a></li>
-               <li><a href="#">Projects</a></li>
-               <li><a href="#">Contact</a></li>
-            </ul>
-         </div>
+         <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+         </ul>
+         <FontAwesomeIcon className="nav-button" icon={faBars} />
       </nav>
    )
 }
