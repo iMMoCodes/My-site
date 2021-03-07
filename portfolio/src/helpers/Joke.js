29a566ef089e52bd795c08e2dfb46acd4ec06f52
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-07 18:05:10 +0200

    created contact me page

--- portfolio/src/helpers/Joke.js
+++ portfolio/src/helpers/Joke.js
@@ -4,17 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSmileBeam } from '@fortawesome/free-regular-svg-icons';
 
 const Joke = () => {
-
+   // States
    const [questionText,setQuestionText] = useState("Click to fetch a joke to brighten your day");
    const [progJoke,setProgJoke] = useState("");
    const [jokePartOne,setJokePartOne] = useState("");
    const [jokePartTwo,setJokePartTwo] = useState("");
-
+   // Handle Click
    const handleJoke = () => {
       getJoke();
       setQuestionText("Get me another one !");
    }
-
+   // Fetch Joke
    const getJoke = async () => {
       const response = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist,explicit");
       const data = await response.json();
@@ -31,12 +31,12 @@ const Joke = () => {
 
    return (
       <div className="joke">
-         <p>{progJoke}</p>
-         <p>{jokePartOne}</p>
-         <p>{jokePartTwo}</p>
          <div className="joke-button">
          <button onClick={handleJoke}>{questionText} <FontAwesomeIcon icon={faSmileBeam}/></button>
          </div>
+         <p>{progJoke}</p>
+         <p>{jokePartOne}</p>
+         <p>{jokePartTwo}</p>
       </div>
    )
 }
