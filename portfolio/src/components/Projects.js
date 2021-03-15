7d4654cb644b40fb30b9e374ac8eeb906999c061
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-15 17:28:08 +0200

    changed first project to trivia game and next project coming soon

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,123 +1,223 @@
 import React, { useState } from 'react'
 
+import triviaImage from '../assets/img/trivia.png'
+import shoppingImage from '../assets/img/shopping.png'
+import ticTacToeImage from '../assets/img/game.png'
 
-import recipeImage from "../assets/img/recipe.png";
-import shoppingImage from "../assets/img/shopping.png";
-import ticTacToeImage from "../assets/img/game.png";
-
-import { info } from "../constants/project-info"; 
+import { info } from '../constants/project-info'
 
 const Projects = () => {
+	const {
+		titleOne,
+		titleTwo,
+		titleThree,
+		descOne,
+		descTwo,
+		descThree,
+		liveLinkOne,
+		liveLinkTwo,
+		liveLinkThree,
+		gitHubOne,
+		gitHubTwo,
+		gitHubThree,
+		html,
+		css,
+		js,
+		react,
+		projectOneLearnings,
+		projectTwoLearnings,
+		projectThreeLearnings,
+	} = info
 
-   const {titleOne,titleTwo,titleThree,descOne,descTwo,descThree,liveLinkOne,liveLinkTwo,liveLinkThree,gitHubOne,gitHubTwo,gitHubThree,html,css,js,react, projectOneLearnings, projectTwoLearnings, projectThreeLearnings} = info;
+	// States
+	const [title, setTitle] = useState(titleOne)
+	const [description, setDescription] = useState(descOne)
+	const [stackOne, setStackOne] = useState(react)
+	const [stackTwo, setStackTwo] = useState(css)
+	const [stackThree, setStackThree] = useState(js)
+	const [projectImg, setProjectImg] = useState(triviaImage)
+	const [liveLink, setLiveLink] = useState(liveLinkOne)
+	const [gitHubLink, setGitHubLink] = useState(gitHubOne)
+	const [projectLearn, setProjectLearn] = useState(projectOneLearnings)
 
-   // States
-   const [title, setTitle] = useState(titleOne);
-   const [description, setDescription] = useState(descOne);
-   const [stackOne, setStackOne] = useState(react);
-   const [stackTwo, setStackTwo] = useState(css);
-   const [stackThree, setStackThree] = useState(js);
-   const [projectImg, setProjectImg] = useState(recipeImage);
-   const [liveLink, setLiveLink] = useState(liveLinkOne);
-   const [gitHubLink, setGitHubLink] = useState(gitHubOne);
-   const [projectLearn, setProjectLearn] = useState(projectOneLearnings);
+	// Handle Button Clicks
+	const handleProjectOne = () => {
+		setTitle(titleOne)
+		setDescription(descOne)
+		setStackOne(react)
+		setStackTwo(null)
+		setStackThree(null)
+		setProjectImg(triviaImage)
+		setLiveLink(liveLinkOne)
+		setGitHubLink(gitHubOne)
+		setProjectLearn(projectOneLearnings)
+	}
+	// Handle Button Clicks
+	const handleProjectTwo = () => {
+		setTitle(titleTwo)
+		setDescription(descTwo)
+		setStackOne(html)
+		setStackTwo(css)
+		setStackThree(js)
+		setProjectImg(shoppingImage)
+		setLiveLink(liveLinkTwo)
+		setGitHubLink(gitHubTwo)
+		setProjectLearn(projectTwoLearnings)
+	}
+	// Handle Button Clicks
+	const handleProjectThree = () => {
+		setTitle(titleThree)
+		setDescription(descThree)
+		setStackOne(html)
+		setStackTwo(css)
+		setStackThree(js)
+		setProjectImg(ticTacToeImage)
+		setLiveLink(liveLinkThree)
+		setGitHubLink(gitHubThree)
+		setProjectLearn(projectThreeLearnings)
+	}
 
-   // Handle Button Clicks
-   const handleProjectOne = () => {
-      setTitle(titleOne);
-      setDescription(descOne);
-      setStackOne(react);
-      setStackTwo(css);
-      setStackThree(js);
-      setProjectImg(recipeImage);
-      setLiveLink(liveLinkOne);
-      setGitHubLink(gitHubOne);
-      setProjectLearn(projectOneLearnings);
-   }
-   // Handle Button Clicks
-   const handleProjectTwo = () => {
-      setTitle(titleTwo);
-      setDescription(descTwo);
-      setStackOne(html);
-      setStackTwo(css);
-      setStackThree(js);
-      setProjectImg(shoppingImage);
-      setLiveLink(liveLinkTwo);
-      setGitHubLink(gitHubTwo);
-      setProjectLearn(projectTwoLearnings);
-   }
-   // Handle Button Clicks
-   const handleProjectThree = () => {
-      setTitle(titleThree);
-      setDescription(descThree);
-      setStackOne(html);
-      setStackTwo(css);
-      setStackThree(js);
-      setProjectImg(ticTacToeImage);
-      setLiveLink(liveLinkThree);
-      setGitHubLink(gitHubThree);
-      setProjectLearn(projectThreeLearnings);
-   }
-
-   return (
-      <main className="projects" id="projects">
-         <h1>Projects</h1>
-         <div className="project-choice">
-            {/* Project Choosing Buttons */}
-            <button onClick={handleProjectOne}>{titleOne}</button>
-            <button onClick={handleProjectTwo}>{titleTwo}</button>
-            <button onClick={handleProjectThree}>{titleThree}</button>
-         </div>
-         <section className="project-container">
-            <div className="project-text-area">
-               {/* Project Info */}
-               <h2>{title}</h2>
-               <h3>{description}</h3>
-               <div className="project-info">
-                  <div className="project-learned">
-                     <h3>What I learned from this project</h3>
-                     <ul>
-                        <li>{projectLearn.first}</li>
-                        <li>{projectLearn.second}</li>
-                        <li>{projectLearn.third}</li>
-                        <li>{projectLearn.fourth}</li>
-                        <li>{projectLearn.fifth}</li>
-                        <li>{projectLearn.sixth}</li>
-                        <li>{projectLearn.seventh}</li>
-                     </ul>
-                  </div>
-                  {/* Project Stack */}
-                     <div className="project-stack">
-                        <h3>Stacks used</h3>
-                        <ul>
-                           <li>{stackOne}</li>
-                           <li>{stackTwo}</li>
-                           <li>{stackThree}</li>
-                        </ul>
-                     </div>
-               </div>
-               {/* Live and Code Buttons */}
-               <div className="project-buttons">
-                  <a target="_blank" rel="noreferrer" href={liveLink}>View Live</a>
-                  <a target="_blank" rel="noreferrer" href={gitHubLink}>View Code</a>
-               </div>
-            </div>
-            {/* Project Image */}
-            <div className="project-img-area">
-               <img className="project-img" src={projectImg} alt="project"/>
-            </div>
-         </section>
-         <div className="my-current-project">
-            <h1>My current project</h1>
-            <h3>To view my current work project click on the buttons below</h3>
-            <div className="my-current-project-buttons">
-            {/* <a target="_blank" rel="noreferrer" href="https://github.com/iMMoCodes/sudoku">View Live</a> */}
-            {/* <a target="_blank" rel="noreferrer" href="https://github.com/iMMoCodes/sudoku" >View Code</a> */}
-            <p>I'm trying to make my next project a bit more of my own and advanced so need to do some studying... Coming soon!</p>
-            </div>
-         </div>
-      </main>
-   )
+	return (
+		<main className='projects' id='projects'>
+			<h1>Projects</h1>
+			<div className='project-choice'>
+				{/* Project Choosing Buttons */}
+				<button onClick={handleProjectOne}>{titleOne}</button>
+				<button onClick={handleProjectTwo}>{titleTwo}</button>
+				<button onClick={handleProjectThree}>{titleThree}</button>
+			</div>
+			<section className='project-container'>
+				<div className='project-text-area'>
+					{/* Project Info */}
+					<h2>{title}</h2>
+					<h3>{description}</h3>
+					<div className='project-info'>
+						<div className='project-learned'>
+							<h3>
+								What
+								I
+								learned
+								from
+								this
+								project
+							</h3>
+							<ul>
+								<li>
+									{
+										projectLearn.first
+									}
+								</li>
+								<li>
+									{
+										projectLearn.second
+									}
+								</li>
+								<li>
+									{
+										projectLearn.third
+									}
+								</li>
+								<li>
+									{
+										projectLearn.fourth
+									}
+								</li>
+								<li>
+									{
+										projectLearn.fifth
+									}
+								</li>
+								<li>
+									{
+										projectLearn.sixth
+									}
+								</li>
+								<li>
+									{
+										projectLearn.seventh
+									}
+								</li>
+							</ul>
+						</div>
+						{/* Project Stack */}
+						<div className='project-stack'>
+							<h3>
+								Stacks
+								used
+							</h3>
+							<ul>
+								<li>
+									{
+										stackOne
+									}
+								</li>
+								<li>
+									{
+										stackTwo
+									}
+								</li>
+								<li>
+									{
+										stackThree
+									}
+								</li>
+							</ul>
+						</div>
+					</div>
+					{/* Live and Code Buttons */}
+					<div className='project-buttons'>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href={
+								liveLink
+							}
+						>
+							View Live
+						</a>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href={
+								gitHubLink
+							}
+						>
+							View Code
+						</a>
+					</div>
+				</div>
+				{/* Project Image */}
+				<div className='project-img-area'>
+					<img
+						className='project-img'
+						src={projectImg}
+						alt='project'
+					/>
+				</div>
+			</section>
+			<div className='my-current-project'>
+				<h1>My current project</h1>
+				<h3>To view my current work project click on the buttons below</h3>
+				<h4>Next project coming soon</h4>
+				{/* <div className='my-current-project-buttons'>
+					<a
+						target='_blank'
+						rel='noreferrer'
+						href='https://immocodes-trivia.netlify.app/'
+					>
+						View Live
+					</a>
+					<a
+						target='_blank'
+						rel='noreferrer'
+						href='https://github.com/iMMoCodes/trivia'
+					>
+						View Code
+					</a>
+				</div> */}
+			</div>
+		</main>
+	)
 }
 
 export default Projects
--- portfolio/src/constants/project-info.js
+++ portfolio/src/constants/project-info.js
@@ -1,43 +1,45 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faCss3, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faCss3, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 
 export const info = {
-   titleOne: "Recipe Search",
-   titleTwo: "Shopping Cart",
-   titleThree: "Tic Tac Toe",
-   descOne: "Type in food and you can find ingredients needed to make it.",
-   descTwo: "Basic shopping site with working shopping cart.",
-   descThree: "A game of Tic Tac Toe for two.",
-   liveLinkOne: "https://my-recipe-search-app-live.netlify.app/",
-   liveLinkTwo: "https://my-shopping-cart-app.netlify.app/",
-   liveLinkThree: "https://my-tac-tic-toc-game.netlify.app/",
-   gitHubOne: "https://github.com/iMMoCodes/Recipe-search/tree/main/recipe-app",
-   gitHubTwo: "https://github.com/iMMoCodes/Shopping-Cart",
-   gitHubThree: "https://github.com/iMMoCodes/Tic-Tac-Toe",
-   html: <FontAwesomeIcon style={{color: "#e34c26"}} icon={faHtml5}/>,
-   css: <FontAwesomeIcon style={{color: "#264de4"}} icon={faCss3}/>,
-   js: <FontAwesomeIcon style={{color: "#f0db4f"}} icon={faJsSquare}/>,
-   react: <FontAwesomeIcon style={{color: "#61DBFB"}} icon={faReact}/>,
-   projectOneLearnings: {
-      first: "Basic React",
-      second: "How to use useState",
-      third: "How to use useEffect",
-      fourth: "How to fetch data from an API",
-      fifth: "How to work with JSON"
-   },
-   projectTwoLearnings: {
-      first: "How to use async / await",
-      second: "How to use fetch",
-      third: "How to use classes in JS",
-      fourth: "How to use local / session storage",
-      fifth: "How to use try / catch",
-      sixth: "Higher order functions",
-      seventh: "How 'this' works (still not an expert)"
-   },
-   projectThreeLearnings: {
-      first: "Basic CSS",
-      second: "CSS variables",
-      third: "CSS Pseudo elements & Pseudo classes",
-      fourth: "Basic coding logic in JS"
-   }
+	titleOne: 'Trivia Game',
+	titleTwo: 'Shopping Cart',
+	titleThree: 'Tic Tac Toe',
+	descOne: 'A four player trivia game with multiple difficulties and categories.',
+	descTwo: 'Basic shopping site with working shopping cart.',
+	descThree: 'A game of Tic Tac Toe for two.',
+	liveLinkOne: 'https://immocodes-trivia.netlify.app/',
+	liveLinkTwo: 'https://my-shopping-cart-app.netlify.app/',
+	liveLinkThree: 'https://my-tac-tic-toc-game.netlify.app/',
+	gitHubOne: 'https://github.com/iMMoCodes/trivia',
+	gitHubTwo: 'https://github.com/iMMoCodes/Shopping-Cart',
+	gitHubThree: 'https://github.com/iMMoCodes/Tic-Tac-Toe',
+	html: <FontAwesomeIcon style={{ color: '#e34c26' }} icon={faHtml5} />,
+	css: <FontAwesomeIcon style={{ color: '#264de4' }} icon={faCss3} />,
+	js: <FontAwesomeIcon style={{ color: '#f0db4f' }} icon={faJsSquare} />,
+	react: <FontAwesomeIcon style={{ color: '#61DBFB' }} icon={faReact} />,
+	projectOneLearnings: {
+		first: 'Basic React',
+		second: 'How to use useState',
+		third: 'How to use useEffect',
+		fourth: 'How to fetch data from an API',
+		fifth: 'How to work with JSON',
+		sixth: 'Use of props',
+		seventh: 'Folder structure',
+	},
+	projectTwoLearnings: {
+		first: 'How to use async / await',
+		second: 'How to use fetch',
+		third: 'How to use classes in JS',
+		fourth: 'How to use local / session storage',
+		fifth: 'How to use try / catch',
+		sixth: 'Higher order functions',
+		seventh: "How 'this' works (still not an expert)",
+	},
+	projectThreeLearnings: {
+		first: 'Basic CSS',
+		second: 'CSS variables',
+		third: 'CSS Pseudo elements & Pseudo classes',
+		fourth: 'Basic coding logic in JS',
+	},
 }
