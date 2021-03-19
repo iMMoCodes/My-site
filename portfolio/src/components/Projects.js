26189aeaebfa03eb76dce7e9eec2cd2a3d27ae63
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-19 15:23:10 +0200

    changed current project

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -93,128 +93,54 @@ const Projects = () => {
 					<h3>{description}</h3>
 					<div className='project-info'>
 						<div className='project-learned'>
-							<h3>
-								What
-								I
-								learned
-								from
-								this
-								project
-							</h3>
+							<h3>What I learned from this project</h3>
 							<ul>
-								<li>
-									{
-										projectLearn.first
-									}
-								</li>
-								<li>
-									{
-										projectLearn.second
-									}
-								</li>
-								<li>
-									{
-										projectLearn.third
-									}
-								</li>
-								<li>
-									{
-										projectLearn.fourth
-									}
-								</li>
-								<li>
-									{
-										projectLearn.fifth
-									}
-								</li>
-								<li>
-									{
-										projectLearn.sixth
-									}
-								</li>
-								<li>
-									{
-										projectLearn.seventh
-									}
-								</li>
+								<li>{projectLearn.first}</li>
+								<li>{projectLearn.second}</li>
+								<li>{projectLearn.third}</li>
+								<li>{projectLearn.fourth}</li>
+								<li>{projectLearn.fifth}</li>
+								<li>{projectLearn.sixth}</li>
+								<li>{projectLearn.seventh}</li>
 							</ul>
 						</div>
 						{/* Project Stack */}
 						<div className='project-stack'>
-							<h3>
-								Stacks
-								used
-							</h3>
+							<h3>Stacks used</h3>
 							<ul>
-								<li>
-									{
-										stackOne
-									}
-								</li>
-								<li>
-									{
-										stackTwo
-									}
-								</li>
-								<li>
-									{
-										stackThree
-									}
-								</li>
+								<li>{stackOne}</li>
+								<li>{stackTwo}</li>
+								<li>{stackThree}</li>
 							</ul>
 						</div>
 					</div>
 					{/* Live and Code Buttons */}
 					<div className='project-buttons'>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href={
-								liveLink
-							}
-						>
+						<a target='_blank' rel='noreferrer' href={liveLink}>
 							View Live
 						</a>
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href={
-								gitHubLink
-							}
-						>
+						<a target='_blank' rel='noreferrer' href={gitHubLink}>
 							View Code
 						</a>
 					</div>
 				</div>
 				{/* Project Image */}
 				<div className='project-img-area'>
-					<img
-						className='project-img'
-						src={projectImg}
-						alt='project'
-					/>
+					<img className='project-img' src={projectImg} alt='project' />
 				</div>
 			</section>
 			<div className='my-current-project'>
 				<h1>My current project</h1>
 				<h3>To view my current work project click on the buttons below</h3>
-				<h4>Next project coming soon</h4>
-				{/* <div className='my-current-project-buttons'>
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://immocodes-trivia.netlify.app/'
-					>
+				<h4>A page where you can register,login and add users to your contacts</h4>
+				<div className='my-current-project-buttons'>
+					<a target='_blank' rel='noreferrer' href='https://immocontacts.netlify.app'>
 						View Live
 					</a>
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://github.com/iMMoCodes/trivia'
-					>
+					<a target='_blank' rel='noreferrer' href='https://github.com/iMMoCodes/contact'>
 						View Code
 					</a>
-				</div> */}
+				</div>
 			</div>
 		</main>
 	)
